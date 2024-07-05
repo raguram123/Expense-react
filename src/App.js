084a6d97cb6f0271a951688631764e35a55f1ecf
Raguraman B <137371586+raguram123@users.noreply.github.com>
2024-07-05 17:05:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ExpenseTracker from './ExpenseTracker';
+
+function App() {
+  return (
+    <div className="App">
+     <ExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
